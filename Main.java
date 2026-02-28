8c06625119074d122bdd65850f61ff9770d69f14
JohntSmith <jonathan.tsmith14@gmail.com>
2026-02-28 22:17:23 +0000

    SELF DRIVING CARS OPINION

--- Main.java
+++ Main.java
@@ -1,18 +1,38 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
-// COLLABORATORS:
-// DATE:  
+// ReadyForSubmission=Y
+// YOUR NAME: Jonathan Smith
+// COLLABORATORS: N/A
+// DATE: 2/28/2025
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("My thoughts on the article are:");
+		System.out.println("\n\nMy thoughts on the article are:");
 		// Part 2: Invoking/calling method
-		
+		printEthicsReflections();
+		System.out.println("With all that being said, I'm excited for the era of self driving cars and the new problems to overcome!\n");
+
 	}
 	
 	// Part 1: Creating method definition
+	// Part 3: Identifying elements of the method
+	// Method Header: public static void printEthicsReflections()
+	// Return type: void
+	// Method identifier: printEthicsReflections
+	public static void printEthicsReflections() 
+	{
+	//Method body: everything within these curly braces.
+	System.out.println("\u001B[1;4m1.) Priority (Passengers vs. Pedestrians):\u001B[0m");
+	System.out.print("-Cars should prioritize humans.");
+	System.out.println("(I know... Easier said than done.)");
+	System.out.println("But, maybe we can dial it down to the type of insurance that we want!");
+	System.out.println("I for one, know that I would prefer the computer to protect whoever is in the car, \u001B[1mespecially\u001B[0m if there was a child in the car.");
 	
+	System.out.println("\n\u001B[1;4m2.) Algorithmic Factors:\u001B[0m");
+	System.out.println("-Factors should include speed, road conditions, minimizing total harm, and preferred type of insurance.");
+
+	System.out.println("\n\u001B[1;4m3.) Decision Makers:\u001B[0m");
+	System.out.println("-Algorithms should be decided by a mix of ethics, safety regulations, and passenger priorities.\n");
+	}	
 }
